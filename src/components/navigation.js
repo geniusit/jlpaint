637827c61257e7f6b5b52447bb337a9fbe9db547
Bertrand <bertrand.deweer@gmail.com>
2020-11-14 23:11:20 +0100

    ok

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -5,7 +5,7 @@ import ThemeChanger from "../components/themeChanger"
 export default (props) => (
   <nav className="navigation"> 
     {/* <Link to="/contact">Contact</Link> */}
-    <ThemeChanger/>
+    {/* <ThemeChanger/> */}
   </nav>
   
 )
